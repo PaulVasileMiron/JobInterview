b836af5120432f794636e73495912d0c7aef25a3
PaulVasileMiron <55735548+PaulVasileMiron@users.noreply.github.com>
2023-06-28 19:32:20 +0200

    Add files via upload

---
+++ src/main/java/interview_challenge/interview_challenfe/App.java
@@ -0,0 +1,11 @@
+package interview_challenge.interview_challenfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
